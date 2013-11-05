84f468ba4bd72beb3677246d275b8e91040e40e4
tbalint <tillman.balint@gmail.com>
2013-11-05 13:36:19 +0100

    ui update

--- src/bwmcts/sparcraft/SparcraftUI.java
+++ src/bwmcts/sparcraft/SparcraftUI.java
@@ -1,11 +1,22 @@
 package bwmcts.sparcraft;
 
+import javabot.types.UnitType.UnitTypes;
+
 import javax.swing.*;
 
 import java.awt.*;
+import java.util.HashMap;
 
 public class SparcraftUI extends JComponent {
 	
+	int offSetX=30;
+	int offSetY=30;
+	
+	
+	HashMap<String,Image> images=new HashMap<String,Image>();
+	Image background;
+	String dirPath="c:\\itu\\Sparcraft\\starcraft_images\\";
+	//Image Terran_Marine;
 	/**
 	 * 
 	 */
@@ -14,22 +25,40 @@ public class SparcraftUI extends JComponent {
 	
 	public SparcraftUI(GameState state) {
 		_state=state;
+		for (UnitTypes u: UnitTypes.values()){
+			System.out.println(u.toString());
+			System.out.println(dirPath+u.toString()+".png");
+			images.put(u.toString(), Toolkit.getDefaultToolkit().getImage(dirPath+"units\\"+u.toString()+".png"));
+		}
+		//Terran_Marine = Toolkit.getDefaultToolkit().getImage("c:\\itu\\Sparcraft\\starcraft_images\\units\\Terran_Marine.png");
+		int  i=(int)(Math.random()*10 % 4); 
+		background=Toolkit.getDefaultToolkit().getImage(dirPath+"ground\\ground"+(i>0?i:"")+".png");
 	}
 	
 	
 	public void paint(Graphics g){
 		Map map=_state.getMap();
 		if (map!=null){
-			g.drawRect(50, 50, map.getPixelWidth(), map.getPixelHeight());
+			if (background !=null){
+				g.drawImage(background, offSetX, offSetY, map.getPixelWidth(), map.getPixelHeight(),this);
+			} else {
+				g.drawRect(offSetX, offSetY, map.getPixelWidth(), map.getPixelHeight());
+			}
+			
 		}
+		
 		g.setColor(Color.blue);
 		for (Unit a : _state.getAllUnit()[0]){
 			if (a!=null && a.isAlive()){
-				g.drawOval(a.pos().getX()+48, a.pos().getY()+48, 4, 4);
-				//if (a._previousAction!=null && a._previousAction._moveType==UnitActionTypes.ATTACK){
-				//	System.out.println(a.previousAction().toString());
-				//	g.drawLine(a.pos().getX()-2, a.pos().getY()-2, a._previousAction.pos().getX()-2, a._previousAction.pos().getY()-2);
-				//}
+				Image i=images.get(a.type().getName().replaceAll(" ", "_"));
+				if (i!=null){
+					drawImageOnPosition(g,i,a.pos());
+			    } else{
+			    	g.drawOval(a.pos().getX()-2+offSetX, a.pos().getY()-2+offSetY, 4, 4);
+			    }
+				if (a._previousAction!=null && a._previousAction._moveType==UnitActionTypes.ATTACK){
+					g.drawLine(a.pos().getX()-2+offSetX, a.pos().getY()-2+offSetY, a._previousAction.pos().getX()-2+offSetX, a._previousAction.pos().getY()-2+offSetY);
+				}
 					
 			}
 			
@@ -37,10 +66,15 @@ public class SparcraftUI extends JComponent {
 		g.setColor(Color.red);
 		for (Unit a : _state.getAllUnit()[1]){
 			if (a!=null && a.isAlive()){
-				g.drawOval(a.pos().getX()+48, a.pos().getY()+48, 4, 4);
-				//if (a._previousAction!=null && a._previousAction._moveType==UnitActionTypes.ATTACK){
-				//	g.drawLine(a.pos().getX()-2, a.pos().getY()-2, a._previousAction.pos().getX()-2, a._previousAction.pos().getY()-2);
-				//}
+				Image i=images.get(a.type().getName().replaceAll(" ", "_"));
+				if (i!=null){
+					drawImageOnPosition(g,i,a.pos());
+			    } else {
+			    	g.drawOval(a.pos().getX()-2+offSetX, a.pos().getY()-2+offSetY, 4, 4);
+			    }
+				if (a._previousAction!=null && a._previousAction._moveType==UnitActionTypes.ATTACK){
+					g.drawLine(a.pos().getX()-2+offSetX, a.pos().getY()-2+offSetY, a._previousAction.pos().getX()-2+offSetX, a._previousAction.pos().getY()-2+offSetY);
+				}
 			}
 			
 		} 
@@ -48,5 +82,12 @@ public class SparcraftUI extends JComponent {
 	
 	public void setGameState(GameState state){
 		_state=state;
+	}
+	
+	private void drawImageOnPosition(Graphics g,Image i, Position p){
+		int width=i.getWidth(this);
+		int height=i.getHeight(this);
+
+		g.drawImage(i, p.getX()+offSetX-(int)(width/2), p.getY()+offSetY-(int)(height/2), width, height, this);
 	}
 }
